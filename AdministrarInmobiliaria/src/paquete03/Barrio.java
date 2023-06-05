6b030c1dddf978934d1baaefddaaf7a436449673
renataa <116390061+reeenatamc@users.noreply.github.com>
2023-06-04 23:24:33 -0500

    paquete3

--- AdministrarInmobiliaria/src/paquete03/Barrio.java
+++ AdministrarInmobiliaria/src/paquete03/Barrio.java
@@ -5,10 +5,35 @@
  */
 package paquete03;
 
+import java.io.Serializable;
+
 /**
  *
  * @author reroes
  */
-public class Barrio {
-    
+public class Barrio implements Serializable{
+    private String nombreBarrio, referencia;
+
+    public Barrio(String nombreBarrio, String referencia) {
+        this.nombreBarrio = nombreBarrio;
+        this.referencia = referencia;
+    }
+
+    // Getters y setters
+    public String getNombre() {
+        return nombreBarrio;
+    }
+
+    public void setNombre(String nombreBarrio) {
+        this.nombreBarrio = nombreBarrio;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
 }
+
