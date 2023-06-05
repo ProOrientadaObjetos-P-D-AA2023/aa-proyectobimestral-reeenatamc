b10a7693fc03598802b650c075e2d4263d924ac6
renataa <116390061+reeenatamc@users.noreply.github.com>
2023-06-04 23:23:56 -0500

    paquete2

--- AdministrarInmobiliaria/src/paquete02/Propietario.java
+++ AdministrarInmobiliaria/src/paquete02/Propietario.java
@@ -5,10 +5,46 @@
  */
 package paquete02;
 
+import paquete03.Barrio;
+
+import java.io.Serializable;
+
 /**
  *
  * @author reroes
  */
-public class Propietario {
-    
+public class Propietario implements Serializable{
+    private String nombres, apellidos, identificacion;
+
+    public Propietario(String nombres, String apellidos, String identificacion) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.identificacion = identificacion;
+    }
+
+
+    //setters and getters
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
 }
