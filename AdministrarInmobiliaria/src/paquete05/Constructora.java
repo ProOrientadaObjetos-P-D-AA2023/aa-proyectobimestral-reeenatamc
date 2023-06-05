5dda5705915290171b5b7ef13b553a3d6c2b2557
renataa <116390061+reeenatamc@users.noreply.github.com>
2023-06-04 23:25:39 -0500

    paquete5

--- AdministrarInmobiliaria/src/paquete05/Constructora.java
+++ AdministrarInmobiliaria/src/paquete05/Constructora.java
@@ -5,10 +5,34 @@
  */
 package paquete05;
 
+import java.io.Serializable;
+
 /**
  *
  * @author reroes
  */
-public class Constructora {
-    
+public class Constructora implements Serializable{
+    private String nombreConstructora, idEmpresa;
+
+    public Constructora(String nombreConstructora, String idEmpresa) {
+        this.nombreConstructora = nombreConstructora;
+        this.idEmpresa = idEmpresa;
+    }
+
+    //getters and setters
+    public String getNombreConstructora() {
+        return nombreConstructora;
+    }
+
+    public void setNombreConstructora(String nombreConstructora) {
+        this.nombreConstructora = nombreConstructora;
+    }
+
+    public String getIdEmpresa() {
+        return idEmpresa;
+    }
+
+    public void setIdEmpresa(String idEmpresa) {
+        this.idEmpresa = idEmpresa;
+    }
 }
